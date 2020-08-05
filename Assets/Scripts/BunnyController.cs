9fa1f75c77d8fcf35f84ffe4c0c5cf50132e6dbe
NMITTutor <Todd.Cochrane@nmit.ac.nz>
2020-08-05 15:28:18 +1200

    Playing with Runbunnyrun
    Runbunny Run is a tutorial provided  by Jonathon Ackerman, currently at Clear Point in Acukland NZ. https://www.linkedin.com/in/jonathanackerman/?originalSubdomain=nz
    
    Oopsmay include Kenney's asset they are Creative Commons

---
+++ Assets/Scripts/BunnyController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BunnyController : MonoBehaviour
+{
+    private Rigidbody2D myRidgidBody;
+    public float bunnyJumpForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRidgidBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonUp("Jump"))
+        {
+            myRidgidBody.AddForce(transform.up * bunnyJumpForce);
+        }
+    }
+}
